b551d992d7599f27c4b8fb9840d3bcd3eed424d4
Taylor <tbrown122387@gmail.com>
2021-08-26 18:03:15 -0400

    still playing around

--- src/likelihood.cpp
+++ src/likelihood.cpp
@@ -1,29 +1,69 @@
 #include <RcppEigen.h>
 #include "svol_sisr_hilb.h"
+#include "resamplers.h"
 
 // [[Rcpp::depends(RcppEigen)]]
 
-using Eigen::Map;               	// 'maps' rather than copies 
-using Eigen::MatrixXd;                  // variable size matrix, double precision
-using Eigen::VectorXd;                  // variable size vector, double precision
-using Eigen::SelfAdjointEigenSolver;    // one of the eigenvalue solvers
+
+// #include <Rcpp.h>
+// using namespace Rcpp;
+
+
+using Eigen::Map; 
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
 
 // 1e3 particles, 5 bits 
-using svol_pfilter = pf::filters::svol_sisr_hilb<1000,5,pf::resamplers::sys_hilb_resampler<1000,1,5,double>,double> sisrsvol_hilb;
-//(phi,beta,sigma);
+#define NP 1000
+#define NB 5
+
+// some type aliases
+using svol_pfilter = svol_sisr_hilb<NP,NB,pf::resamplers::sys_hilb_resampler<NP,1,NB,double> , double>; //(phi,beta,sigma);
+
+// uProposal will be (time X (NP+1))
+// first NP columns will be used for state sampling 
+// last column will be used for resampling at each time point
 
 // [[Rcpp::export]]
-VectorXd approxLL(Map<VectorXd> y, Map<VectorXd> thetaProposal, Map<VectorXd> uProposal) {
-  svol_pfilter pf(thetaProposal(0), thetaProposal(1) thetaProposal(2)); // order: phi, beta, sigma
-  
-  return es.eigenvalues();
+VectorXd approxLL(const Map<VectorXd> y, const Map<VectorXd> thetaProposal, const Map<MatrixXd> uProposal) {
+
+  // construct particle filter object
+  svol_pfilter pf(thetaProposal(0), thetaProposal(1), thetaProposal(2)); // order: phi, beta, sigma
+
+  // iterate over the data 
+  double log_like(0.0);
+  // Eigen::Matrix<double,1,1> yt;
+  // std::array<Eigen::Matrix<double,1,1>, NP> uStateTransition;
+  // Eigen::Matrix<double,1,1> uResample;
+  for(int time = 0; time < y.rows(); ++time){
+    
+    // change types of inputs
+    // yt(0) = y(time);
+    // for(unsigned particle = 0; particle < NP; ++particle) {
+    //   uStateTransition[particle] = uProposal.block(time,particle,1,1); 
+    // }
+    // uResample(0) = uProposal(time,NP);
+    
+    // update particle filter and log-likelihood
+    // pf.filter(yt, uStateTransition, uResample);
+    // log_like += pf.getLogCondLike();
+    log_like += 1.0;
+  }
+    
+  //return es.eigenvalues();
+  VectorXd r;
+  r(0) = log_like;
+  return r;
 }
 
 // You can include R code blocks in C++ files processed with sourceCpp
 // (useful for testing and development). The R code will be automatically 
 // run after the compilation.
-//
+
 
 /*** R
-timesTwo(42)
+numTime <- 3
+numParts <- 1000
+u <- matrix(rnorm(numTime*(numParts+1)), ncol = numParts+1)
+approxLL(1:numTime, 1:3, u)
 */
